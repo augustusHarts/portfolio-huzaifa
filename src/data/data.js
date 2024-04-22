b23bc273e96bab463e1fca4ce5e4d064ed0041f6
augustusHarts <augustus.harts@gmail.com>
2024-04-22 22:40:54 +0530

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,98 @@
+import boltotasachchalto from "../assests/boltotasachchalto.jpg";
+import dedenge from "../assests/dedenge.jpg";
+import gangshit from "../assests/gangshit.jpg";
+import ilostmyskintotheearth from "../assests/ilostmyskintotheearth.jpg";
+import kyunahi from "../assests/kyunahi.jpg";
+import mainhukon from "../assests/mainhukon.jpg";
+import nomercy from "../assests/nomercy.jpg";
+import solorider from "../assests/solorider.jpg";
+import straightup from "../assests/straightup.jpg";
+import tasavvur from "../assests/tasavvur.jpg";
+import unstoppable from "../assests/unstoppable.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "I Lost My Skin To The Earth",
+    artist: "",
+    image: ilostmyskintotheearth,
+    link: "https://youtu.be/3EPFp2nSse8?si=6gdHk5p5wrLlFpH2",
+  },
+  {
+    id: 2,
+    name: "Bolto Tasach Chalto",
+    artist: "KyaSeen",
+    image: boltotasachchalto,
+    link: "https://youtu.be/wKAufCTGCwk?si=rLv2wRptTYNQ9QL5",
+  },
+  {
+    id: 3,
+    name: "De Denge",
+    artist: "KyaSeen",
+    image: dedenge,
+    link: "https://youtu.be/wxMpc7nS9cQ?si=z7WKuE-2m48ftXJz",
+  },
+  {
+    id: 4,
+    name: "No Mercy",
+    artist: "OGAnkit",
+    image: nomercy,
+    link: "https://youtu.be/qpB_Y-Fe5NA?si=ZEgTyRKGlXWTA5Tf",
+  },
+  {
+    id: 5,
+    name: "Solo Rider",
+    artist: "OGAnkit",
+    image: solorider,
+    link: "https://youtu.be/ZSW8jR0kv_c?si=3zvtvDtnqJGhLNuD",
+  },
+  {
+    id: 6,
+    name: "Mein Hu Kon",
+    artist: "OGAnkit",
+    image: mainhukon,
+    link: "https://youtu.be/veVnbCQaEF8?si=nv38YkZNA91sLSkh",
+  },
+  {
+    id: 7,
+    name: "Tasavvur",
+    artist: "OGAnkit",
+    image: tasavvur,
+    link: "https://youtu.be/DRjxLyXzzrs?si=_RbqWHf6uThco4nm",
+  },
+  {
+    id: 8,
+    name: "Gang Shit",
+    artist: "OGAnkit",
+    image: gangshit,
+    link: "https://youtu.be/WAFgvgoAByc?si=ocwNcajH21PKZ75N",
+  },
+  {
+    id: 9,
+    name: "Straight up",
+    artist: "OGAnkit",
+    image: straightup,
+    link: "https://youtu.be/KST3LaO2ac8?si=9X360EVvgt1gRzeQ",
+  },
+  {
+    id: 10,
+    name: "Unstoppable",
+    artist: "OGAnkit",
+    image: unstoppable,
+    link: "https://youtu.be/uI3Fi3L67ns?si=wldQn2DYntXueBic",
+  },
+  {
+    id: 11,
+    name: "Kyu Nahi",
+    artist: "OGAnkit",
+    image: kyunahi,
+    link: "https://youtu.be/nNFelpumQVo?si=5My4BuOsZuRJ3qWe",
+  },
+  // {
+  //     id: 12,
+  //     name: "Dushman",
+  //     artist: "MC Naaz",
+  //     image: "",
+  //     link: "https://youtu.be/E3olAfAP7bo?si=qBRiU2WFvuI4irTy",
+  // },
+];
